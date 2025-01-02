36a13d5a3c4e34d1b11603003b67329517f801d0
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-02 12:34:07 +0100

    Added snap to turn function.

--- Assets/Scripts/GameJoystick/JoystickSceneController.cs
+++ Assets/Scripts/GameJoystick/JoystickSceneController.cs
@@ -5,10 +5,14 @@ public class JoystickSceneController : MonoBehaviour
     [SerializeField]
     private float movementSpeed;
 
+    [SerializeField]
+    private float rotationTimeInterval = 0.5f; // min time interval between two rotations
+
     private GameManager gameManager;
     private IInputProvider inputProvider;
     private IControllerInput leftController;
     private IControllerInput rightController;
+    private bool canRotate = true;
 
     public void Initialize(GameManager gameManager)
     {
@@ -40,6 +44,31 @@ public class JoystickSceneController : MonoBehaviour
         movement *= movementSpeed * Time.deltaTime;
 
         inputProvider.GetRigTransform().position += movement;
+        
+        // Snap to turn
+        float rotation = leftController.Joystick.x;
+        if (rotation > 0.5f && canRotate)
+        {
+            Rotate(Vector3.up);
+        }
+        else if (rotation < -0.5f && canRotate)
+        {
+            Rotate(Vector3.down);
+        }
+        
+    }
+
+    private void Rotate(Vector3 direction)
+    {
+        inputProvider.GetRigTransform().Rotate(direction * 90f, Space.World);
+        
+        canRotate = false;
+        Invoke(nameof(ResetRotation), rotationTimeInterval);
+    }
+
+    private void ResetRotation()
+    {
+        canRotate = true;
     }
 }
 
